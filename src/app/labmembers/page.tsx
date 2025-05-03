ee3a30a5279830224c447d0653efaeb33b667a47
Landry Johnston <landryjohnston93@gmail.com>
2025-05-03 13:22:16 -0500

    1st commie

--- src/app/labmembers/page.tsx
+++ src/app/labmembers/page.tsx
@@ -1,47 +1,45 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
 import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
+// Define a type for the member object
+interface Member {
+  member_id: number;
+  member_first_name: string;
+  member_last_name: string;
+  email: string;
+  lab_status: string;
+}
+
 export default function LabMembers() {
+  const [members, setMembers] = useState<Member[]>([]) // Specify the type for the members state
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch('/api/members')
+        const response = await data.json()
+        setMembers(response.members) // Assuming the response contains a 'members' field with an array of members
+        console.log(response.members)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <div className="m-4">
-      <h2 className="text-4xl font-bold mb-6">Lab Members</h2>
-      <section>
-        <h2 className="text-2xl font-bold mb-2">Founder/Director</h2>
-        <p className="mb-6">
-          Founder/Director Information
-        </p>
-      </section>
-
-      <section>
-        <h2 className="text-2xl font-bold mb-2">Faculty Members</h2>
-        <p className="mb-6">
-          Faculty Member Information
-        </p>
-      </section>
-
-      <section>
-        <h2 className="text-2xl font-bold mb-2">Graduate Students</h2>
-        <p className="mb-6">
-          Graduate Student Information
-        </p>
-      </section>
-
-      <section>
-        <h2 className="text-2xl font-bold mb-2">Undergraduate Studetns</h2>
-        <p className="mb-6">
-          Undergraduate Student Information
-        </p>
-      </section>
-
-      <section>
-        <h2 className="text-2xl font-bold mb-2">Alumni Members</h2>
-        <p className="mb-6">
-          Alumni Member Information
-        </p>
-      </section>
+    <div>
+      Lab Members Page
+      {members.map((member) => (
+        <div key={member.member_id}>
+          {member.member_first_name} {member.member_last_name} - {member.lab_status}
+        </div>
+      ))}
     </div>
   );
 }
